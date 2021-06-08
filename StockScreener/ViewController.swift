9d3340e5400e9ff414ae4094765af73fa32335e3
Max Nechaev <maxnechaev.dev@gmail.com>
2021-06-08 21:09:11 +0300

    Initial commit

---
+++ StockScreener/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockScreener
+//
+//  Created by Max Nechaev on 08.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello, World")
+    }
+
+
+}
+
